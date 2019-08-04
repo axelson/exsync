45ff38ec7e6af9ad3a4fda50bf295da8864360b9
Jason Axelson <jason.axelson@gmail.com>
2019-08-04 06:51:32 -1000

    Log out errors specifically

--- lib/exsync/utils.ex
+++ lib/exsync/utils.ex
@@ -1,6 +1,13 @@
 defmodule ExSync.Utils do
+  require Logger
+
   def recomplete do
-    System.cmd("mix", ["compile"], cd: ExSync.Config.app_source_dir())
+    src_dir = ExSync.Config.app_source_dir()
+
+    case System.cmd("mix", ["compile"], cd: src_dir, stderr_to_stdout: true) do
+      {_, 0} -> :ok
+      {output, _} -> log_error(output)
+    end
   end
 
   def unload(module) when is_atom(module) do
@@ -18,5 +25,9 @@ defmodule ExSync.Utils do
     {:ok, binary, _} = :erl_prim_loader.get_file(file)
     module = beam_path |> Path.basename(".beam") |> String.to_atom()
     :code.load_binary(module, file, binary)
+  end
+
+  defp log_error(string) do
+    Logger.error(string)
   end
 end
